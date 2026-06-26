02eb62a4544bb5dc75830ef9a2fa8330e6348e8c
awaaaaja <awaaaaja@gmail.com>
2026-06-27 02:38:18 +0700

    fix: pass empty object to PrismaClient to fix TypeScript compilation

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -7,7 +7,7 @@ declare global {
 
 export const db =
   globalThis.prisma ??
-  new PrismaClient();
+  new PrismaClient({});
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.prisma = db;
